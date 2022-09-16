21384fd3469da6dabb00490abbfe13f4b38fdaa2
Allan C. M. Lira <acmlira@gmail.com>
2022-09-16 09:15:34 -0300

    Add missing annotation

--- src/main/java/acmlira/thecoolestcroupier/TheCoolestCroupierApplication.java
+++ src/main/java/acmlira/thecoolestcroupier/TheCoolestCroupierApplication.java
@@ -2,8 +2,10 @@ package acmlira.thecoolestcroupier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class TheCoolestCroupierApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TheCoolestCroupierApplication.class, args);
